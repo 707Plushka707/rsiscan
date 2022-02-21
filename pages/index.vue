39038b92587d660b0977b4d46aafacdb3a612aff
baotm <trinhminhbao@gmail.com>
2022-02-22 06:01:04 +0700

    asd

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,11 @@
             <!-- Page title actions -->
             <div class="col-auto ms-auto d-print-none">
               <div class="btn-list">
-                <a href="#" class="btn btn-primary d-none d-sm-inline-block">
+                <a
+                  href="#"
+                  v-b-modal.modalCauHinh
+                  class="btn btn-primary d-none d-sm-inline-block"
+                >
                   <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
                   Cấu Hình
                 </a>
@@ -27,75 +31,99 @@
       </div>
 
       <div class="page-body">
-        <div class="container-xl">
+        <div class="p-2">
           <div class="row row-deck row-cards">
             <div class="col-12">
               <div class="card">
                 <div class="table-responsive">
                   <b-row>
                     <b-col cols="12">
-                      <b-form inline>
-                        <label>Coin</label>
-                        <b-form-input
-                          style="text-transform: uppercase"
-                          v-model="filterName"
-                          class="m-2"
-                          debounce="400"
-                          type="search"
-                        ></b-form-input>
-                        <label>RSI 15M</label>
-                        <b-form-input
-                          style="text-transform: uppercase"
-                          v-model="rsi15mRule"
-                          class="m-2"
-                          debounce="400"
-                          type="search"
-                        ></b-form-input>
-                        <label>RSI 5M</label>
-                        <b-form-input
-                          style="text-transform: uppercase"
-                          v-model="rsi5mRule"
-                          class="m-2"
-                          debounce="400"
-                          type="search"
-                        ></b-form-input>
-                        <label>Volume</label>
-                        <b-form-input
-                          style="text-transform: uppercase"
-                          v-model="volumeRule"
-                          class="m-2"
-                          debounce="400"
-                          type="search"
-                        ></b-form-input>
-                      </b-form>
+                      <div class="mx-auto">
+                        <b-form inline>
+                          <label>Coin</label>
+                          <b-form-input
+                            style="text-transform: uppercase"
+                            v-model="filterName"
+                            class="m-2"
+                            debounce="400"
+                            type="search"
+                          ></b-form-input>
+                          <label>RSI 15M</label>
+                          <b-form-input
+                            style="text-transform: uppercase"
+                            v-model="rsi15mRule"
+                            class="m-2"
+                            debounce="400"
+                            type="search"
+                          ></b-form-input>
+                          <label>RSI 5M</label>
+                          <b-form-input
+                            style="text-transform: uppercase"
+                            v-model="rsi5mRule"
+                            class="m-2"
+                            debounce="400"
+                            type="search"
+                          ></b-form-input>
+                          <label>Volume</label>
+                          <b-form-input
+                            style="text-transform: uppercase"
+                            v-model="volumeRule"
+                            class="m-2"
+                            debounce="400"
+                            type="search"
+                          ></b-form-input>
+                        </b-form>
+                      </div>
+                    </b-col>
+                    <b-col col lg="9" md="12" sm="12">
+                      <b-table
+                        outlined
+                        head-variant="dark"
+                        class="text-center"
+                        striped
+                        hover
+                        :items="getPairlist"
+                        :fields="fields"
+                        show-empty
+                        :sort-by.sync="sortBy"
+                        :sort-desc.sync="sortDesc"
+                        :tbody-transition-props="transProps"
+                        :filter="filterName"
+                      >
+                        <template #empty="">
+                          <p>
+                            Không có thông tin phù hợp với các trường lọc lúc
+                            này!
+                          </p>
+                        </template>
+                        <template #cell(#)="data">
+                          {{ data.index + 1 }}
+                        </template>
+                      </b-table>
+                    </b-col>
+                    <b-col col lg="3" md="12" sm="12">
+                      <b-table
+                        outlined
+                        head-variant="dark"
+                        class="text-center"
+                        striped
+                        hover
+                        :items="binanceWatchList"
+                        :fields="fieldsBinanceWatchlist"
+                        show-empty
+                        :sort-by.sync="sortBy"
+                        :sort-desc.sync="sortDesc"
+                        :tbody-transition-props="transProps"
+                      >
+                        <template #empty="">
+                          <p>Chưa có dữ liệu...</p>
+                        </template>
+                        <template #cell(#)="data">
+                          {{ data.index + 1 }}
+                        </template>
+                      </b-table>
                     </b-col>
                   </b-row>
-
-                  <div v-if="dataReady" class="table-responsive">
-                    <b-table
-                      outlined
-                      head-variant="dark"
-                      class="text-center"
-                      striped
-                      hover
-                      :items="getPairlist"
-                      :fields="fields"
-                      show-empty
-                      :sort-by.sync="sortBy"
-                      :sort-desc.sync="sortDesc"
-                      :tbody-transition-props="transProps"
-                      :filter="filterName"
-                    >
-                      <template #empty="">
-                        <p>
-                          Không có thông tin phù hợp với các trường lọc lúc này!
-                        </p>
-                      </template>
-                      <template #cell(#)="data">
-                        {{ data.index + 1 }}
-                      </template>
-                    </b-table>
-                  </div>
                 </div>
               </div>
             </div>
@@ -103,6 +131,88 @@
         </div>
       </div>
     </div>
+
+    <b-modal id="modalCauHinh" title="Cấu Hình Server">
+      <b-form-row>
+        <b-col cols="6">
+          <b-form-group
+            label="Binance Delay"
+            description="Thời gian delay các vòng để tránh bị BAN IP từ binance "
+          >
+            <b-form-input
+              v-model="serverConfig.delayScript"
+              type="number"
+              required
+            ></b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col cols="6">
+          <b-form-group
+            label="Chu Kì RSI"
+            description="Cách tính RSI dựa trên chu kì, ví dụ RSI(6), RSI(14)"
+          >
+            <b-form-input
+              type="number"
+              v-model="serverConfig.period"
+              required
+            ></b-form-input>
+          </b-form-group>
+        </b-col>
+      </b-form-row>
+
+      <b-form-group
+        label="Pair List"
+        description="Danh sách các cặp giao dịch, cách nhau bằng dấu , "
+      >
+        <b-form-textarea
+          rows="6"
+          v-model="serverConfig.pairList"
+          required
+        ></b-form-textarea>
+      </b-form-group>
+      <b-form-row>
+        <b-col cols="6">
+          <b-form-group
+            label="RSI 15M"
+            description="Cấu hình lọc RSI 15M nặc định"
+          >
+            <b-form-input
+              v-model="serverConfig.rsi15m"
+              type="number"
+              min="0"
+              max="100"
+              required
+            ></b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col cols="6">
+          <b-form-group
+            label="RSI 5M"
+            description="Cấu hình lọc RSI 5M mặc định"
+          >
+            <b-form-input
+              type="number"
+              min="0"
+              max="100"
+              v-model="serverConfig.rsi5m"
+              required
+            ></b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col cols="6">
+          <b-form-group
+            label="Volume"
+            description="Cấu hình lọc Volume mặc định"
+          >
+            <b-form-input
+              type="number"
+              v-model="serverConfig.volume"
+              required
+            ></b-form-input>
+          </b-form-group>
+        </b-col>
+      </b-form-row>
+    </b-modal>
     <footer class="footer footer-transparent d-print-none">
       <div class="container-xl">
         <div class="row text-center align-items-center flex-row-reverse">
@@ -139,13 +249,21 @@ export default {
   name: "IndexPage",
   data() {
     return {
+      serverConfig: {
+        delayScript: 20,
+        period: 6,
+        pairList: [],
+        rsi15m: 50,
+        rsi5m: 20,
+        volume: 1,
+      },
       sortBy: "rsi15m.RSI",
       sortDesc: true,
       transProps: {
         name: "flip-list",
       },
       filterName: null,
-      fields: [],
+
       items: [],
       listpair: [],
       dataReady: false,
@@ -179,6 +297,17 @@ export default {
           formatter: (value, key, item) => {
             return this.formatSoTien(parseFloat(value.toString()).toFixed(0));
           },
+        },
+      ],
+      binanceWatchList: [],
+      fieldsBinanceWatchlist: [
+        { key: "#" },
+        { key: "Pair" },
+        {
+          key: "BB",
+        },
+        {
+          key: "Windows",
         },
       ],
     };
