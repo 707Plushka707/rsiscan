c30b1e784a8611fa33fafa3f8f06eff2d5c6adf6
baotm <trinhminhbao@gmail.com>
2022-02-21 22:26:04 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,277 @@
+<template>
+  <div class="wrapper">
+    <header class="navbar">
+      <div class="container-xl"></div>
+    </header>
+    <div class="page-wrapper">
+      <div class="container-xl">
+        <!-- Page title -->
+        <div class="page-header d-print-none">
+          <div class="row align-items-center">
+            <div class="col">
+              <!-- Page pre-title -->
+              <div class="page-pretitle">RSI Scan</div>
+              <h2 class="page-title">Dashboard</h2>
+            </div>
+            <!-- Page title actions -->
+            <div class="col-auto ms-auto d-print-none">
+              <div class="btn-list">
+                <a href="#" class="btn btn-primary d-none d-sm-inline-block">
+                  <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+
+                  Cấu Hình
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="page-body">
+        <div class="container-xl">
+          <div class="row row-deck row-cards">
+            <div class="col-12">
+              <div class="card">
+                <div class="table-responsive">
+                  <b-row>
+                    <b-col cols="11" class="my-1">
+                      <b-form-group
+                        label="RSI 15M"
+                        label-cols="4"
+                        label-align="right"
+                        class="mb-0"
+                      >
+                        <b-input-group>
+                          <b-form-input
+                            v-model="rsi15mRule"
+                            type="search"
+                          ></b-form-input>
+                        </b-input-group>
+                      </b-form-group>
+                    </b-col>
+                  </b-row>
+
+                  <div v-if="dataReady" class="table-responsive">
+                    <b-table
+                      outlined
+                      head-variant="dark"
+                      class="text-center"
+                      striped
+                      hover
+                      :items="listpair"
+                      :fields="fields"
+                      show-empty
+                      :sort-by.sync="sortBy"
+                      :sort-desc.sync="sortDesc"
+                      :tbody-transition-props="transProps"
+                    >
+                      <template #cell(#)="data">
+                        {{ data.index + 1 }}
+                      </template>
+                    </b-table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="footer footer-transparent d-print-none">
+      <div class="container-xl">
+        <div class="row text-center align-items-center flex-row-reverse">
+          <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+            <ul class="list-inline list-inline-dots mb-0">
+              <li class="list-inline-item">
+                Copyright &copy; 2021
+                <a href="." class="link-secondary">Baotm</a>. All rights
+                reserved.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+function compare(a, b) {
+  if (a.rsi && b.rsi) {
+    if (a.rsi > b.rsi) {
+      return -1;
+    }
+    if (a.rsi < b.rsi) {
+      return 1;
+    }
+    return 0;
+  } else {
+    return 0;
+  }
+}
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      sortBy: "rsi15m.RSI",
+      sortDesc: true,
+      transProps: {
+        name: "flip-list",
+      },
+      fields: [],
+      items: [],
+      listpair: [],
+      dataReady: false,
+      fetchStatus: false,
+      rsi5mRule: 0,
+      rsi15mRule: 50,
+      filter: null,
+      filterOn: [],
+      fields: [
+        { key: "#" },
+        { key: "name", sortable: true },
+        {
+          key: "rsi15m.RSI",
+          sortable: true,
+          label: "RSI 15M",
+          sortDirection: "desc",
+        },
+        { key: "rsi5m.RSI", sortable: true, label: "RSI 5M" },
+        {
+          key: "info.close",
+          sortable: true,
+          label: "Price",
+          formatter: (value, key, item) => {
+            return parseFloat(value).toFixed(4);
+          },
+        },
+        {
+          key: "info.volume",
+          sortable: true,
+          label: "Volume",
+          formatter: (value, key, item) => {
+            return parseFloat(value).toFixed(0);
+          },
+        },
+      ],
+    };
+  },
+  computed: {},
+  async mounted() {
+    this.getData();
+    setInterval(() => {
+      this.getData();
+    }, 3000); //chay moi 5p 1 lan
+  },
+  methods: {
+    filter15m(row, filter) {
+      console.log(row);
+      if (row.age >= filter) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+    getPrice(item) {
+      let price = parseFloat(item.info.close);
+      return price.toFixed(4);
+    },
+    getVolume(item) {
+      let volume = parseFloat(item.info.volume);
+      return volume.toFixed(0);
+    },
+    getRSI15M(item) {
+      let result;
+      if (item.rsi15m) {
+        result = item.rsi15m.RSI;
+      } else {
+        result = "x";
+      }
+      return result;
+    },
+
+    getRSI5M(item) {
+      let result;
+      if (item.rsi5m) {
+        result = item.rsi5m.RSI;
+      } else {
+        result = "x";
+      }
+      return result;
+    },
+    getName5M(item) {
+      return 1;
+    },
+    getName15M(item) {
+      let result;
+      if (item[0].time === "15m") {
+        result = item[0];
+      } else {
+        return "";
+      }
+      console.log(result);
+      return result.RSI;
+    },
+    filterRSI() {},
+    getData() {
+      // this.dataReady = false;
+      console.log("fetch...");
+      fetch("http://localhost:3000/rsi")
+        .then((data) => data.json())
+        .then((data) => {
+          //  console.log(data)
+          let list = [];
+          data.forEach((element) => {
+            if (element) {
+              list.push(element);
+            }
+          });
+          list = list.sort(compare);
+          list = _.groupBy(list, "name");
+
+          let newList = [];
+          _.forOwn(list, (value, key) => {
+            let rsi15;
+            let rsi5;
+            let d = value;
+            for (let i = 0; i < 2; i++) {
+              if (d[i]) {
+                if (d[i].time === "5m") {
+                  rsi5 = d[i];
+                }
+                if (d[i].time === "15m") {
+                  rsi15 = d[i];
+                }
+              } else {
+                if (rsi15) {
+                  rsi5 = null;
+                }
+                if (rsi5) {
+                  rsi15 = null;
+                }
+              }
+            }
+
+            let newObject = {
+              name: key,
+              rsi15m: rsi15,
+              rsi5m: rsi5,
+              ...value[0],
+            };
+            if (rsi15) {
+              newList.push(newObject);
+            }
+          });
+
+          this.listpair = newList;
+          this.dataReady = true;
+        });
+    },
+  },
+};
+</script>
+<style scoped>
+table .flip-list-move {
+  transition: transform 1s;
+}
+</style>
