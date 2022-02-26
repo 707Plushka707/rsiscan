e1bc667ba6b1dae8a46dde252b4d7726b68540ac
baotm <trinhminhbao@gmail.com>
2022-02-26 14:35:22 +0700

    asd

--- pages/dual.vue
+++ pages/dual.vue
@@ -1,68 +1,57 @@
 <template>
-  <b-container fluid>
-    <b-row>
-      <b-col cols="1"></b-col>
-      <b-col cols="11">
-        <div id="listgroup-ex" style="position: relative; overflow-y: auto">
-          <div class="tradingview" id="list-item-1">
-            <h1>BTC</h1>
-            <iframe class="" src="https://www.binance.com/en/futures/CVCUSDT">
-            </iframe>
-          </div>
-          <div class="tradingview" id="list-item-2">
-            <h1>ETH</h1>
-            <iframe src="https://www.binance.com/en/futures/CVCUSDT"> </iframe>
-          </div>
-          <div class="tradingview" id="list-item-3">
-            <h1>LTC</h1>
-            <iframe src="https://www.binance.com/en/futures/CVCUSDT"> </iframe>
-          </div>
-          <div class="tradingview" id="list-item-4">
-            <h1>DOGE</h1>
-            <iframe src="https://www.binance.com/en/futures/CVCUSDT"> </iframe>
-          </div>
-          <div class="tradingview" id="list-item-5">
-            <h1>SHIT</h1>
-            <iframe src="https://www.binance.com/en/futures/CVCUSDT"> </iframe>
-          </div>
-        </div>
-      </b-col>
-    </b-row>
-  </b-container>
+  <div class="grid-container">
+    <div
+      class="item"
+      v-for="(item, index) in listPair"
+      :key="index"
+      v-bind:style="listPair.length / 2 == 1 ? 'height:90vh' : 'height:48vh'"
+    >
+      <iframe class="frame" :src="item.url"> </iframe>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      text: `
-          Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla
-          tempor. Laborum consequat non elit enim exercitation cillum aliqua
-          consequat id aliqua. Esse ex consectetur mollit voluptate est in duis
-          laboris ad sit ipsum anim Lorem. Incididunt veniam velit elit elit veniam
-          Lorem aliqua quis ullamco deserunt sit enim elit aliqua esse irure. Laborum
-          nisi sit est tempor laborum mollit labore officia laborum excepteur
-          commodo non commodo dolor excepteur commodo. Ipsum fugiat ex est consectetur
-          ipsum commodo tempor sunt in proident.
-        `,
+      listPair: [],
     };
+  },
+  mounted() {
+    let listPair = this.$cookies.get("watchList");
+    listPair = JSON.parse(listPair);
+    let myList = [];
+
+    listPair.map((item) => {
+      let urlStr = `https://www.tradingview.com/widgetembed/?frameElementId=tradingview_9db2f&symbol=${item}PERP&interval=D&symboledit=1&saveimage=1&toolbarbg=f1f3f6&studies=%5B%5D&theme=dark&style=1&timezone=Etc%2FUTC&studies_overrides=%7B%7D&overrides=%7B%7D&enabled_features=%5B%5D&disabled_features=%5B%5D&locale=vi_VN`;
+      myList.push({
+        name: item,
+        url: urlStr,
+      });
+    });
+    this.listPair = myList;
   },
 };
 </script>
 <style scoped>
-.traddingview {
-  height: 100%;
-  widows: 100%;
+html {
+  background-color: black !important;
 }
-iframe {
-  height: 650px;
+.grid-container {
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 10px;
+  background-color: black;
+  padding: 10px;
+}
+
+.grid-container > div {
+  background-color: rgba(12, 12, 12, 0.8);
+  text-align: center;
+}
+.frame {
   width: 100%;
-}
-.list {
-  position: absolute;
-  top: 0%;
-  left: 0%;
-  width: 150px;
-  height: 150px;
+  height: 100%;
 }
 </style>
